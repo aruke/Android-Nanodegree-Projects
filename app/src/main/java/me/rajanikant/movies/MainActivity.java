97b7daefcede08af366e1be3c236e7928edebc47
Rajanikant Deshmukh <rajanikantdeshmukh@gmail.com>
2016-03-27 12:09:10 +0530

    Add butterknife dependency

--- app/src/main/java/me/rajanikant/movies/MainActivity.java
+++ app/src/main/java/me/rajanikant/movies/MainActivity.java
@@ -1,21 +1,24 @@
 package me.rajanikant.movies;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.actiity_main_toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.inject(this);
         setSupportActionBar(toolbar);
     }
 
