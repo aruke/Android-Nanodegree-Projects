85a0b869a154f434f5f22e53c72e4114717f5ba5
Rajanikant Deshmukh <rajanikantdeshmukh@gmail.com>
2016-06-11 13:43:15 +0530

    Added 2 pane layout for main activity

--- app/src/main/java/me/rajanikant/movies/ui/MainActivity.java
+++ app/src/main/java/me/rajanikant/movies/ui/MainActivity.java
@@ -3,9 +3,13 @@ package me.rajanikant.movies.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.widget.FrameLayout;
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.Optional;
 import me.rajanikant.movies.Constants;
 import me.rajanikant.movies.R;
 import me.rajanikant.movies.api.model.Movie;
@@ -15,25 +19,68 @@ public class MainActivity extends AppCompatActivity implements OnFragmentInterac
 
     private static final String TAG = "MainActivity";
 
+    @Optional
+    @InjectView(R.id.activity_main_container_layout)
+    FrameLayout detailsFragmentContainer;
+
+    private boolean twoPanedLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
+
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        float dpHeight = displayMetrics.heightPixels / displayMetrics.density;
+        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+
+        Log.w(TAG, "onCreate: height in DPI " + dpHeight);
+        Log.w(TAG, "onCreate: width in DPI " + dpWidth);
+
+        if (detailsFragmentContainer!=null) {
+            Log.d(TAG, "onCreate: switched to 2 paned layout");
+            twoPanedLayout = true;
+        }
+        else {
+            Log.d(TAG, "onCreate: Still in single pane");
+            twoPanedLayout = false;
+        }
     }
 
     @Override
     public void onMovieItemClicked(Movie movie) {
         Log.d(TAG, "onMovieItemClicked: Movie item clicked with title " + movie.getTitle());
-        Intent detailIntent = new Intent(this, MovieDetailsActivity.class);
-        detailIntent.putExtra(Constants.INTENT_EXTRA_MOVIE_ID, movie.getId());
-        detailIntent.putExtra(Constants.INTENT_EXTRA_MOVIE_TITLE, movie.getTitle());
-        detailIntent.putExtra(Constants.INTENT_EXTRA_MOVIE_OVERVIEW, movie.getOverview());
-        detailIntent.putExtra(Constants.INTENT_EXTRA_MOVIE_RATINGS, movie.getVoteAverage());
-        detailIntent.putExtra(Constants.INTENT_EXTRA_MOVIE_RELEASE_DATE, movie.getReleaseDate());
-        detailIntent.putExtra(Constants.INTENT_EXTRA_MOVIE_POSTER_PATH, movie.getPosterPath());
-        detailIntent.putExtra(Constants.INTENT_EXTRA_MOVIE_BACKDROP_PATH, movie.getBackdropPath());
-        startActivity(detailIntent);
+
+        int id = movie.getId();
+        String title = movie.getTitle();
+        String overview = movie.getOverview();
+        String releaseDate = movie.getReleaseDate();
+        double ratings = movie.getVoteAverage();
+        String posterPath = movie.getPosterPath();
+        String backdropPath = movie.getBackdropPath();
+
+        if (twoPanedLayout){
+
+            MovieDetailsFragment fragment = MovieDetailsFragment.newInstance(id, title, overview, releaseDate, ratings, backdropPath, posterPath);
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.activity_main_container_layout, fragment)
+                    .commit();
+
+        } else {
+
+            Intent detailIntent = new Intent(this, MovieDetailsActivity.class);
+            detailIntent.putExtra(Constants.INTENT_EXTRA_MOVIE_ID, id);
+            detailIntent.putExtra(Constants.INTENT_EXTRA_MOVIE_TITLE, title);
+            detailIntent.putExtra(Constants.INTENT_EXTRA_MOVIE_OVERVIEW, overview);
+            detailIntent.putExtra(Constants.INTENT_EXTRA_MOVIE_RATINGS, ratings);
+            detailIntent.putExtra(Constants.INTENT_EXTRA_MOVIE_RELEASE_DATE, ratings);
+            detailIntent.putExtra(Constants.INTENT_EXTRA_MOVIE_POSTER_PATH, posterPath);
+            detailIntent.putExtra(Constants.INTENT_EXTRA_MOVIE_BACKDROP_PATH, backdropPath);
+            startActivity(detailIntent);
+        }
     }
 
 
