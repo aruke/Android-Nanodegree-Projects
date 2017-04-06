33314bf37f679a42215e5130bb97a287e3ef0739
Rajanikant Deshmukh <rajanikantdeshmukh@gmail.com>
2017-04-06 23:20:27 +0900

    Make text more readable

--- app/src/main/java/me/rajanikant/xyzreader/ui/ArticleDetailFragment.java
+++ app/src/main/java/me/rajanikant/xyzreader/ui/ArticleDetailFragment.java
@@ -167,6 +167,7 @@ public class ArticleDetailFragment extends Fragment implements
                             + mCursor.getString(ArticleLoader.Query.AUTHOR)
                             + "</font>"));
             bodyView.setText(Html.fromHtml(mCursor.getString(ArticleLoader.Query.BODY)));
+            bodyView.setMovementMethod(LinkMovementMethod.getInstance());
             ImageLoaderHelper.getInstance(getActivity()).getImageLoader()
                     .get(mCursor.getString(ArticleLoader.Query.PHOTO_URL), new ImageLoader.ImageListener() {
                         @Override
